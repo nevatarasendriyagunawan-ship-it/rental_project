882aea15cd6eddec69c8a5a502bb80af8abd2c3f
nevatarasendriyagunawan-ship-it <nevatarasendriyagunawan@gmail.com>
2025-12-10 17:20:55 +0700

    Delete rental_mobil_skanega directory

--- rental_mobil_skanega/admin/user_hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../koneksi.php';
-$id = $_GET['id'];
-mysqli_query($koneksi, "DELETE  FROM user where user_id = '$id'");
-echo "<script>alert('Yakin ingin di hapus?'); window.location.href='user.php'</script>";
-?>
